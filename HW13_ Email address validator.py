c07256e904cdc6daf724fb13314f7f7999a58ce7
shpakova <shpakovaanastasiia@gmail.com>
2024-05-01 18:56:16 +0300

    Fixed H13

--- HW13_ Email address validator.py
+++ HW13_ Email address validator.py
@@ -1,24 +1,24 @@
 # Email address validator
 
-#email = "aaa@bbb.ccc"
+email = "aaa@bbb.ccc"
+#email = "@aaabbbccc"
+#email = "aaabbbccc."
 #email = "@aaabbbccc."
-email = "!aa@bbb.c>c"
+#email = "!aa@bbb.c>c"
 #email = "a@a@bbb.ccc"
+#email = "aa.bbb@ccc"
 #email = "aa.a@bbb.ccc"
 #email = "@aaa.bb$b@cc.l@^c."
-result = False
 
-if not email.startswith("@"):
-    if not email.endswith("."):
-        if len(email.split("@")) == 2 and len(email.split(".")) == 2:
-            if email.find("@") < email.find("."):
-                for symbol in email:
-                    if not symbol.isalpha():
-                        if not symbol.isdigit():
-                            if symbol == "@" or symbol == ".":
-                                result = True
-                            else:
-                                result = False
+result = True
+starts_ends_with_symbol = email.startswith("@") and email.endswith(".")
+symbol_used_once = len(email.split("@")) == 2 and len(email.split(".")) == 2
+at_before_dot = email.find("@") < email.find(".")
+
+if not starts_ends_with_symbol and symbol_used_once and at_before_dot:
+    for symbol in email:
+        if not symbol.isalpha() and not symbol.isdigit() and symbol != "@" and symbol != ".":
+            result = False
+else:
+    result = False
 print(result)
-
-
