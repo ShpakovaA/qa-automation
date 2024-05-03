155f495a4c00b6346c8beedfa641db45cfe623cc
shpakova <shpakovaanastasiia@gmail.com>
2024-05-03 11:36:10 +0300

    Code change for HW17

--- HW17_Sorting the columns of a two-dimensional list.py
+++ HW17_Sorting the columns of a two-dimensional list.py
@@ -7,11 +7,12 @@ lst = [['a', 'c', 'e'],
 
 for column in range(0, len(lst[0]), 1):
     lst_sorted = sorted([elements[column] for elements in lst])
-    for index in range(0, len(lst),1):
+    #for index in range(0, len(lst),1):
+    for index in range(0, len(lst), 1):
         lst[index][column] = lst_sorted[index]
 
-for i in range(0, len(lst), 1):
-    print(lst[i])
+for row in lst:
+    print(row)
 
 
 # # ->
