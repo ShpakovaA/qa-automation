f838d7418fb407902333738f561dae2e69002912
shpakova <shpakovaanastasiia@gmail.com>
2024-05-20 11:08:30 +0300

    Updated HW30

--- HW30_Time schedule.py
+++ HW30_Time schedule.py
@@ -8,6 +8,6 @@ day = timedelta(hours=24)
 
 while lecture <= 32:
     if start.weekday() == 0 or start.weekday() == 3:
-        print(f"Lecture {lecture:>2}: {start.strftime("%d %b %Y %H:%M")}")
+        print(f"Lecture {lecture:>2}: {start.strftime('%d %b %Y %H:%M')}")
         lecture += 1
     start += day
