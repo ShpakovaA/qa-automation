3641eab466e393bcfc9c00e481614127400b7be5
shpakova <shpakovaanastasiia@gmail.com>
2024-05-21 20:09:24 +0300

    Updated HW34

--- HW34_Custom zip.py
+++ HW34_Custom zip.py
@@ -1,26 +1,20 @@
 def custom_zip(*sequences, full=False, default=None) -> list:
     result = []
-
     if full:
         length_ = max(len(sequence) for sequence in sequences)
+        for sequence in sequences:
+                while len(sequence) < length_:
+                    sequence.append(default)
     else:
         length_ = min(len(sequence) for sequence in sequences)
 
-    for sequence in sequences:
-        if len(sequence) < length_:
-            while len(sequence) < length_:
-                sequence.append(default)
-
     for i in range(length_):
-        result.append([*[sequence[i] for sequence in sequences]])
+        result.append(tuple([*[sequence[i] for sequence in sequences]]))
 
-    for i in range(len(result)):
-        result[i] = tuple(result[i])
-
-    print(result)
+    return result
 
 seq1 = [1, 2, 3, 4, 5]
 seq2 = [9, 8, 7]
 
-custom_zip(seq1, seq2)  # [(1, 9), (2, 8), (3, 7)]
-custom_zip(seq1, seq2, full=True, default="Q")  # [(1, 9), (2, 8), (3, 7), (4, 'Q'), (5, 'Q')]
+print(custom_zip(seq1, seq2))  # [(1, 9), (2, 8), (3, 7)]
+print(custom_zip(seq1, seq2, full=True, default="Q")) # [(1, 9), (2, 8), (3, 7), (4, 'Q'), (5, 'Q')]
