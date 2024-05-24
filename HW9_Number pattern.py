adc6191e8a4a25bd117433342e2a9a7fc6425170
shpakova <shpakovaanastasiia@gmail.com>
2024-05-24 10:27:49 +0300

    Updated HW9

--- HW9_Number pattern.py
+++ HW9_Number pattern.py
@@ -6,14 +6,21 @@ while count <= n:
     spaces = n - count
     count_numbers = count-1
 
-    for space in range(0,spaces):
-        print(" ", end="")
+    for space in range(0, spaces):
+        print(" ", end=" ")
 
     for number in range(1, count+1):
-        print(number, end="")
+        if count == 1:
+            print(number, end="")
+        else:
+            print(number, end=" ")
 
     while count_numbers >= 1:
-        print(count_numbers, end="")
+        if count_numbers == 1:
+            print(count_numbers, end="")
+        else:
+            print(count_numbers, end=" ")
+
         count_numbers -= 1
 
     print()
