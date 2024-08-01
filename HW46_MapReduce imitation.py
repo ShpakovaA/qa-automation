06b8243aa4e3da7e120b204722946c474729c17a
shpakova <shpakovaanastasiia@gmail.com>
2024-08-01 13:44:00 +0300

    errors fix

--- HW46_MapReduce imitation.py
+++ HW46_MapReduce imitation.py
@@ -64,18 +64,29 @@ def letter_counter_in_n_threads(directory, letter_to_find, number_of_threads):
     directory_files = os.listdir(directory)
     files_in_directory = len(directory_files)
     files_per_thread = max(1, round(files_in_directory / number_of_threads))
+    files_for_threads = []
 
-    # files_for_threads = []
-    # for i in range(0, files_in_directory, files_per_thread):
-    #     files_for_threads.append(directory_files[i:i+files_per_thread])
-    files_for_threads = [directory_files[i:i+files_per_thread] for i in range(0, files_in_directory, files_per_thread)]
+    for i in range(0, files_in_directory, files_per_thread):
+        files = directory_files[i:i + files_per_thread]
+        files_qty = len(files)
+
+        if files_qty < files_per_thread:
+            for n in range(files_qty):
+                files_for_threads[n].append(files[n])
+        else:
+            files_for_threads.append(files)
 
     result = []
+    threads = []
 
-    for i in range(len(files_for_threads)):
-        thread = Thread(target=count_in_files, args=(directory, files_for_threads[i], letter_to_find, result))
+    for files in files_for_threads:
+        thread = Thread(target=count_in_files, args=(directory, files, letter_to_find, result))
+        threads.append(thread)
         thread.start()
+
+    for thread in threads:
         thread.join()
+
     return sum(result)
 
 
@@ -86,9 +97,8 @@ def func_execution_time(func, *params):
     return end_time - start_time
 
 
-file_generator("new_dir", 50, 100)
+file_generator("new_dir", 10, 20)
 print(letter_counter_in_one_thread_1("new_dir", "B"))
-print(func_execution_time(letter_counter_in_one_thread_1, "new_dir", "B"))
 print()
-print(letter_counter_in_n_threads("new_dir", "B", 150))
-print(func_execution_time(letter_counter_in_n_threads, "new_dir", "B", 150))
+print(letter_counter_in_n_threads("new_dir", "B", 4))
+
